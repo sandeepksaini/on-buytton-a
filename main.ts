4cfb47266074724b314169e1e4ec8889268db051
Sandeep Kumar <sandeeprhct@gmail.com>
2026-09-09 19:30:01 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 9; index++) {
+        devices.tellCameraTo(MesCameraEvent.TakePhoto)
+        basic.showLeds(`
+            . . . . .
+            # # # . .
+            . # . . .
+            . # . . .
+            # # # . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # # #
+            `)
+        basic.showLeds(`
+            . # . . #
+            . # . . #
+            . # # # #
+            . . # # .
+            . . # # .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . . . # .
+            . # # # .
+            . # . # .
+            . # # # #
+            `)
+        basic.showIcon(IconNames.Heart)
+        devices.tellCameraTo(MesCameraEvent.TakePhoto)
+        for (let index = 0; index < 4; index++) {
+            music.setBuiltInSpeakerEnabled(true)
+            pins.analogWritePin(AnalogPin.P0, 100)
+            led.setBrightness(9e+98)
+            music.setVolume(9e+100)
+        }
+    }
+})
+input.onSound(DetectedSound.Loud, function () {
+    music.play(music.stringPlayable("G F G A - F E D ", 40), music.PlaybackMode.InBackground)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . # . #
+        # . # # .
+        # # # # .
+        . . . . #
+        # # # # #
+        `)
+    music.play(music.stringPlayable("G F G A - F E D ", 120), music.PlaybackMode.UntilDone)
+})
